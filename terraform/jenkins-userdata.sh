ec30a7d92cd743d8f3b2a56fa0da30846773bc4d
yousefsalemW <yousefsalem.w@gmail.com>
2026-08-12 00:20:02 +0300

    feat(jenkins): install Trivy in userdata

--- terraform/jenkins-userdata.sh
+++ terraform/jenkins-userdata.sh
@@ -5,19 +5,20 @@ cd /tmp
 
 apt-get update
 apt-get install -y \
-    openjdk-21-jdk \
-    docker.io \
-    maven \
-    unzip \
-    curl \
-    wget
+  openjdk-21-jdk \
+  docker.io \
+  maven \
+  unzip \
+  curl \
+  wget
 
 # Jenkins
+
 wget -O /usr/share/keyrings/jenkins-keyring.asc \
-https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
-> /etc/apt/sources.list.d/jenkins.list
+  > /etc/apt/sources.list.d/jenkins.list
 
 apt-get update
 apt-get install -y jenkins
@@ -27,14 +28,29 @@ systemctl enable --now docker
 systemctl enable --now jenkins
 
 # kubectl
+
 curl -LO "https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -m 0755 kubectl /usr/local/bin/kubectl
 
 # Helm
+
 curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 # AWS CLI v2
+
 curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o awscliv2.zip
 unzip -q awscliv2.zip
 ./aws/install
 
+# Trivy
+
+apt-get install -y wget gnupg
+
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key \
+  | gpg --dearmor -o /usr/share/keyrings/trivy.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" \
+  | tee /etc/apt/sources.list.d/trivy.list
+
+apt-get update
+apt-get install -y trivy

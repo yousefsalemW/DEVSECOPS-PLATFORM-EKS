126b9d8cb5833ea728432bd0e2908bd0064452f8
yousefsalemW <yousefsalem.w@gmail.com>
2026-08-11 23:11:08 +0300

    Update EKS Kubernetes version to 1.34

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = "vprofile-eks"
-  cluster_version = "1.30"
+  cluster_version = "1.34"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
